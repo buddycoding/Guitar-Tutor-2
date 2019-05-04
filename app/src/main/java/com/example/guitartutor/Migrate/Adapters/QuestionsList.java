e7d3df2e79dc1d384809be51bd07323ed9c34907
buddycoding <dummyemail12313123@gmail.com>
2019-05-04 09:36:37 +0800

    bug fixes

--- app/src/main/java/com/example/guitartutor/Migrate/Adapters/QuestionsList.java
+++ app/src/main/java/com/example/guitartutor/Migrate/Adapters/QuestionsList.java
@@ -25,8 +25,8 @@ public class QuestionsList {
         chords.add(new QuestionsList("e", new int[]{ 0, 2, 2, 1, 0, 0 }, new int[]{ 0, 2, 3, 1, 0, 0 }));
         chords.add(new QuestionsList("g", new int[]{ 3,2,0,0,0,3 }, new int[]{ 2,1,0,0,0,3 }));
 
-        //chords.add(new QuestionsList("c", new int[]{ -1,3,2,0,1,0 }, new int[]{ -1,3,2,0,1,0 })); orig
-        chords.add(new QuestionsList("c", new int[]{ 0,1,0,0,0,0 }, new int[]{ 0,1,0,0,0,0 }));
+        chords.add(new QuestionsList("c", new int[]{ -1,3,2,0,1,0 }, new int[]{ -1,3,2,0,1,0 })); //orig
+        //chords.add(new QuestionsList("c", new int[]{ 0,1,0,0,0,0 }, new int[]{ 0,1,0,0,0,0 }));
         chords.add(new QuestionsList("em", new int[]{ 0,2,2,0,0,0 }, new int[]{ 0,1,2,0,0,0 }));
         chords.add(new QuestionsList("am", new int[]{ -1,0,2,2,1,0 }, new int[]{ -1,0,2,3,1,0 }));
         chords.add(new QuestionsList("a", new int[]{ -1,0,2,2,2,0 }, new int[]{ -1,0,2,1,3,0 }));
